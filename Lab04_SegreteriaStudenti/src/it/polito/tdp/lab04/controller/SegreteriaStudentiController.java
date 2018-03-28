3bf81c18fc49730f2c33f49acc177ac01207d66b
carme <carme@DESKTOP-S5J1IBQ.homenet.telecomitalia.it>
2018-03-28 12:48:49 +0200

    Initial commit

--- Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/controller/SegreteriaStudentiController.java
+++ Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/controller/SegreteriaStudentiController.java
@@ -1,5 +1,64 @@
 package it.polito.tdp.lab04.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class SegreteriaStudentiController {
+
+    @FXML
+    private ComboBox<?> menuCorsi;
+
+    @FXML
+    private Button btnCercaIscritti;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtCognome;
+
+    @FXML
+    private Button btnCercaCorsi;
+
+    @FXML
+    private Button btnIscrivi;
+
+    @FXML
+    private TextArea txtAreaStudenti;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void cercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cercaIscritti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selezionaCorsi(ActionEvent event) {
+
+    }
 
 }
